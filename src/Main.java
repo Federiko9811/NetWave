5225d39a81a54928cd7c6ac09886108028ee39fd
Federico Raponi <fraponi98@gmail.com>
2022-04-30 17:22:20 +0200

    Inserimento Dipendente

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import entità.Dipendente;
+
+import static Query.Test.inserisciDipendente;
 import static Query.Test.testQuery;
 
 public class Main {
     public static void main(String[] args) {
+        Dipendente dipendente = new Dipendente(
+                "XRHMLC75M55H783A",
+                "Fabio",
+                "Verdi",
+                "01/01/1995",
+                "fabioverdi@gmail.com",
+                "3332223331",
+                "IT98S0300203280637625659932",
+                null
+        );
+        inserisciDipendente(dipendente);
         testQuery();
     }
 }
--- src/Query/Test.java
+++ src/Query/Test.java
@@ -1,6 +1,7 @@
 package Query;
 
 import db_connection.Connector;
+import entità.Dipendente;
 
 import java.sql.*;
 
@@ -11,11 +12,10 @@ public class Test {
         ResultSet rs = null;
 
         try {
-            String sql = "SELECT * FROM dipendente where nome=?";
+            String sql = "SELECT * FROM dipendente";
             assert link != null;
             ps = link.prepareStatement(sql);
-            ps.setString(1, "Federico");
-            
+
             rs = ps.executeQuery();
 
             int colonne = rs.getMetaData().getColumnCount();
@@ -26,6 +26,33 @@ public class Test {
                 }
                 System.out.println("------------");
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void inserisciDipendente(Dipendente dipendente) {
+        Connection link = Connector.connect();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = "insert into " +
+                    "dipendente (codice_fiscale, nome, cognome, data_nascita, email, telefono, iban, documento) " +
+                    "values (?, ?, ?, ?::date, ?, ?, ?, ?::bytea)";
+            assert link != null;
+            ps = link.prepareStatement(sql);
+            ps.setString(1, dipendente.getCodiceFiscale());
+            ps.setString(2, dipendente.getNome());
+            ps.setString(3, dipendente.getCognome());
+            ps.setString(4, dipendente.getDataNascita());
+            ps.setString(5, dipendente.getEmail());
+            ps.setString(6, dipendente.getTelefono());
+            ps.setString(7, dipendente.getIban());
+            ps.setString(8, dipendente.getDocumento());
+
+            ps.execute();
+            System.out.println("Dipendente inserito con successo");
         } catch (SQLException e) {
             e.printStackTrace();
         }
