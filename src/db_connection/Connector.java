6802aa9e2e2afbc6af86938a70f1ac61b651a640
Federico Raponi <fraponi98@gmail.com>
2022-05-19 20:50:08 +0200

    Cambio Db

--- src/db_connection/Connector.java
+++ src/db_connection/Connector.java
@@ -9,9 +9,9 @@ public class Connector {
         try {
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(
-                    "jdbc:postgresql://netwave.cmz3s5fvdd1s.us-east-1.rds.amazonaws.com:5432/postgres",
-                    "postgres",
-                    "netwave2022"
+                    "jdbc:postgresql://netwave-server.postgres.database.azure.com:5432/postgres",
+                    "postgres@netwave-server",
+                    "Netwave2022@"
             );
         } catch (Exception e) {
             e.printStackTrace();
